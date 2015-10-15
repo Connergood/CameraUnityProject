f9476887d75128861ae2e3377576d8df9b10f755
Conner Quentin Westover <westoverconner@gmail.com>
2015-10-15 04:32:27 -0400

    Added Pressure Pads

---
+++ CameraProject/Assets/Scripts/PressurePad.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePad : MonoBehaviour {
+
+	[SerializeField] GameObject self;
+	[SerializeField] GameObject cubeToMove;
+
+	[SerializeField] float translateX;
+	[SerializeField] float translateY;
+	[SerializeField] float rotate;
+
+	public enum Type{
+		weighted,
+		oneTime
+	}
+	public Type type;
+
+	private bool activated;
+
+	Vector3 initialCubeLocation;
+
+	// Use this for initialization
+	void Start () {
+		initialCubeLocation = cubeToMove.transform.position;
+		activated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.transform.tag == "Player" && type == Type.oneTime){
+			if (!activated){
+				Vector3 location = new Vector3(initialCubeLocation.x + translateX,
+				                               initialCubeLocation.y + translateY,
+				                               0.0f);
+				cubeToMove.transform.position = location;
+				cubeToMove.transform.RotateAround(cubeToMove.transform.position, new Vector3(0.0f, 0.0f, 1.0f),rotate);
+				activated = true;
+				self.transform.localScale = new Vector3(self.transform.localScale.x, self.transform.localScale.y/2, self.transform.localScale.z);
+			}
+		}
+	}
+}
