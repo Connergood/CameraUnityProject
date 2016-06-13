3bd9eed29288cb4d8c0539706afc0036c6e94e74
Conner <cqw1484@g.rit.edu>
2016-06-13 14:30:15 -0400

    Player can grab objects with Q button. All moveable objects have this ability just gotta click the bool isGrabable

---
+++ CameraProject/Assets/Scripts/GrabAbleObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabAbleObject : MonoBehaviour {
+
+    Vector2 difference;
+    GameObject Player;
+    [SerializeField] bool isGrabbable = false;
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+        state = State.unlocked;
+	}
+	
+    enum State
+    {
+        locked,
+        unlocked
+    }
+
+    State state;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrabbable)
+        {
+            if (Input.GetKeyDown(KeyCode.Q) &&
+                !Player.GetComponent<PlayerControl>().playerHidden &&
+                Mathf.Abs(difference.x) < 1 + transform.localScale.x/2 &&
+                Mathf.Abs(difference.y) < 1 + transform.localScale.y/2)
+            {
+                state = State.locked;
+                print("I am locked");
+            }
+            else if (Input.GetKeyUp(KeyCode.Q))
+            {
+                state = State.unlocked;
+            }
+
+            if (state == State.locked)
+            {
+                transform.position = new Vector3(Player.transform.position.x + difference.x, Player.transform.position.y + difference.y, 0.00f);
+            }
+            else
+            {
+                difference = new Vector2(transform.position.x - Player.transform.position.x,
+                                                  transform.position.y - Player.transform.position.y);
+            }
+        }
+    }
+}
