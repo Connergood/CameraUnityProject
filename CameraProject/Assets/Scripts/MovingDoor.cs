cdbcd79b9d88a6311c8bfa7dd375eb76463960b2
Conner <cqw1484@g.rit.edu>
2016-06-27 14:05:33 -0400

    Bouncing Object

---
+++ CameraProject/Assets/Scripts/MovingDoor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingDoor : MonoBehaviour {
+
+    public float initialVelocityX;
+    public float InitialVelocityY;
+    Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start () {
+        myBody = this.gameObject.GetComponent<Rigidbody2D>();
+        myBody.velocity = new Vector2(initialVelocityX, InitialVelocityY);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        Vector2 norm = hit.contacts[0].normal;
+        print(norm);
+        if (Mathf.Abs(norm.x) > Mathf.Abs(norm.y))
+        {
+            if (norm.x > 0)
+            {
+                myBody.velocity = new Vector2(initialVelocityX, myBody.velocity.y);
+            } else
+            {
+                myBody.velocity = new Vector2(-initialVelocityX, myBody.velocity.y);
+            }
+        } else
+        {
+            if(norm.y > 0)
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, InitialVelocityY);
+            } else
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, -InitialVelocityY);
+            }
+        }
+        print(myBody.velocity);
+    }
+}
